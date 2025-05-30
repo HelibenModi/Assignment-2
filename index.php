9d59a43d4040fb4c986b79e576bf61b1873268ce
HelibenModi <helimodi710@gmail.com>
2025-05-30 18:08:06 +0000

    redirecting to register and login page

--- index.php
+++ index.php
@@ -1,8 +1,11 @@
+<!DOCTYPE html>
 <html>
-  <head>
-    <title>PHP Test</title>
-  </head>
-  <body>
-    <?php echo '<p>Hello World</p>'; ?> 
-
+<head>
+    <title>Welcome</title>
+</head>
+<body>
+    <h1>Welcome to the User System</h1>
+    <p><a href="register.php">Create Account</a></p>
+    <p><a href="login.php">Login</a></p>
+</body>
 </html>

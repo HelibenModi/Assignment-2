5a3ad53e28730b1099c54b537e73ffea4738388a
HelibenModi <helimodi710@gmail.com>
2025-05-30 18:10:36 +0000

    User login page created

---
+++ login.php
@@ -0,0 +1,26 @@
+<?php
+require_once('user.php');
+session_start();
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  $username = $_POST['username'];
+  $password = $_POST['password'];
+
+  $user = new User();
+  if ($user->verify_user($username, $password)) {
+    $_SESSION['username'] = $username;
+    header("Location: dashboard.php");
+    exit();
+  } else {
+    echo "Invalid credentials.";
+  }
+}
+?>
+
+<form method="POST">
+  <label>Username:</label><br>
+  <input type="text" name="username" required><br>
+  <label>Password:</label><br>
+  <input type="password" name="password" required><br>
+  <button type="submit">Login</button>
+</form>

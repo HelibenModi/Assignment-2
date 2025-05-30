3729c90c669c90ee24076022ef5bbd6405f11e98
HelibenModi <helimodi710@gmail.com>
2025-05-30 16:46:06 +0000

    Configuration and DB pass added

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+
+define('DB_Host', 'qmb3g.h.filess.io');
+define('DB_User', '4806_spiritmud');
+define('DB_Pass', $_ENV['DB_Pass']);
+define('DB_DATABASE', '4806_spiritmud');
+define('DB_Port', '61000');
+?>
